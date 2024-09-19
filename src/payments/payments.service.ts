25a1fb94ed5d56e16385a2cc0ee439fdc34caf34
williamsmata <williams.rm99@gmail.com>
2024-09-19 14:04:04 -0400

    Create payment session dto

--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -1,29 +1,32 @@
 import { Injectable } from '@nestjs/common';
 import Stripe from 'stripe';
 import { envs } from 'src/config';
+import { PaymentSessionDto } from './dto/payment-session.dto';
 
 @Injectable()
 export class PaymentsService {
   private readonly stripe = new Stripe(envs.stripe_secret);
 
-  async createPaymentSession() {
+  async createPaymentSession(paymentSessionDto: PaymentSessionDto) {
+    const { currency, items } = paymentSessionDto;
+
+    const lineItems = items.map((item) => ({
+      price_data: {
+        currency,
+        product_data: {
+          name: item.name,
+        },
+        unit_amount: Math.round(item.price * 100),
+      },
+      quantity: item.quantity,
+    }));
+
     const session = await this.stripe.checkout.sessions.create({
       mode: 'payment',
       payment_intent_data: {
         metadata: {},
       },
-      line_items: [
-        {
-          price_data: {
-            currency: 'usd',
-            product_data: {
-              name: 'T-shirt',
-            },
-            unit_amount: 2000, // $20.00,
-          },
-          quantity: 2,
-        },
-      ],
+      line_items: lineItems,
       success_url: `http://localhost:3003/payments/success`,
       cancel_url: `http://localhost:3003/payments/cancel`,
     });
