35c5aec7fbe5c2e4d58211cea1bbe8702213c829
williamsmata <williams.rm99@gmail.com>
2024-09-20 10:32:53 -0400

    Update env

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -5,14 +5,16 @@ interface EnvVars {
   PORT: number;
   STRIPE_SECRET: string;
   STRIPE_WEBHOOK_SECRET: string;
-  // DATABASE_URL: string;
+  STRIPE_SUCCESS_URL: string;
+  STRIPE_CANCEL_URL: string;
 }
 
 const envVarsSchema = joi.object<EnvVars>({
   PORT: joi.number().default(3000),
   STRIPE_SECRET: joi.string().required(),
   STRIPE_WEBHOOK_SECRET: joi.string().required(),
-  // DATABASE_URL: joi.string().required(),
+  STRIPE_SUCCESS_URL: joi.string().uri().required(),
+  STRIPE_CANCEL_URL: joi.string().uri().required(),
 });
 
 function validateEnv<T>(
@@ -41,5 +43,6 @@ export const envs: LowerCaseKeys<EnvVars> = {
   port: validatedEnv.PORT,
   stripe_secret: validatedEnv.STRIPE_SECRET,
   stripe_webhook_secret: validatedEnv.STRIPE_WEBHOOK_SECRET,
-  // database_url: validatedEnv.DATABASE_URL,
+  stripe_success_url: validatedEnv.STRIPE_SUCCESS_URL,
+  stripe_cancel_url: validatedEnv.STRIPE_CANCEL_URL,
 };
--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -30,8 +30,8 @@ export class PaymentsService {
         },
       },
       line_items: lineItems,
-      success_url: `http://localhost:3003/payments/success`,
-      cancel_url: `http://localhost:3003/payments/cancel`,
+      success_url: envs.stripe_success_url,
+      cancel_url: envs.stripe_cancel_url,
     });
 
     return session;
